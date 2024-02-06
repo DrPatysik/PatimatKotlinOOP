33c91c602e78afb82673f8d32658dacbe62d7e6d
PATYA <patysik@mail.ru>
2024-02-06 22:24:50 +0300

    добавила compareSomeBoxes , compareSomeBoxesSecond

--- src/main/kotlin/Box.kt
+++ src/main/kotlin/Box.kt
@@ -1,9 +1,19 @@
 class Box(var width: Double, var length: Double, var height: Double) {
+    private var width1 = 0.0
+    private var length1 = 0.0
+    private var height1 = 0.0
+    constructor(width: Double, length: Double, height: Double,
+                _width1: Double, _length1: Double, _height1: Double ):
+                this(width,length,height)
+    {width1 = _width1; height1 = _height1; length1 = _length1 }
+
 
     fun volume(): Double {
         return width * length * height
     }
-
+   private fun volume1(): Double {
+        return width1 * length1 * height1
+    }
     fun compare(box: Box) {
         val thisVolume = volume()
         val boxVolume = box.volume()
@@ -27,6 +37,42 @@ class Box(var width: Double, var length: Double, var height: Double) {
             }
             else{ println("Both boxes  are the same")}
         }
+        fun compareSomeBoxes(vararg boxes:Box) {
+            var boxesArrList:MutableList<Double> = mutableListOf()
+            for(box in boxes) {
+                boxesArrList.add(box.volume()) }
+
+             for(i in boxes ) {
+                 if (boxesArrList.max() == i.volume()) {
+                     println("Box with parameters ${i.width},${i.length},${i.height} is biggest")
+                 }
+
+             }     }
+
+        fun compareSomeBoxesSecond(vararg boxes:Box) {
+            var c = 0.0
+            for (box in boxes) {
+                var a = box.volume()
+                var b = 0.0
+
+                if (a > b) {
+                    c = a
+                } else {
+                    c = b
+                }
+            }
+
+            for( b in boxes) {
+               if( c == b.volume()) {
+                   println("Biggest box have width-${b.width},length-${b.length},height-${b.height}")
+               }
+            }
+        }
     }
 
+
+
+    fun volumeBiggerTwoBoxes(box: Box):Double {
+        return volume1() + volume() + 2.0
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,4 +10,15 @@ fun main(args: Array<String>) {
     val box3 = Box(3.2,4.3,6.4)
     Box.compare2Boxes(box2,myBox)
 
+    val twoBoxes = Box(4.3, 2.3, 3.3,
+                    6.5, 4.5, 1.3)
+    println(twoBoxes.volumeBiggerTwoBoxes(twoBoxes))
+
+    Box.compareSomeBoxes(Box(9.7,5.7,3.5),
+                        Box(4.3,6.5,8.7),
+                        Box(3.6,7.7,8.9))
+
+    Box.compareSomeBoxesSecond(Box(1.2,2.2,3.3) ,
+                                Box(1.1,4.4,5.5))
+
 }
